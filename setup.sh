d356a43782e1aa2827bf6c0be82d179ccc0f0769
Michael Batz <mail@michael-batz.de>
2017-12-30 13:18:19 +0100

    setup.sh: copy directories, create backup of existing dotfiles

--- setup.sh
+++ setup.sh
@@ -21,20 +21,70 @@ cat << "EOF"
 ---------------------------------------------------------------
 EOF
 
-echo $scriptdir
-
 # function copyFile(soure, destination)
 copyFile()
 {
-    # copy file to target
-    echo "copy $1 to $2..."
-    cp $1 $2
+    # define vars
+    sourceFile="${1}"
+    destinationDir="${2}"
+    filename="$(basename "${1}")"
+    destinationFile="${destinationDir}/${filename}"
+
+    echo "file ${filename}:"
+
+    # check if destination file already exist and create backup
+    if [ -f "${destinationFile}" ]
+    then
+        mv -f "${destinationFile}" "${destinationFile}.save"
+        echo "- existing file saved"
+    fi
+
+    # copy source to destination
+    cp "${sourceFile}" "${destinationDir}"
+    echo "- new file copied to ${destinationDir}"
+    echo ""
 }
 
-# walk over all files in ./files directory and copy
+# function copyDir(soure, destination)
+copyDir()
+{
+    # define vars
+    sourceObj="${1}"
+    destinationDir="${2}"
+    objname="$(basename "${sourceObj}")"
+    destinationObj="${destinationDir}/${objname}"
+    
+    echo "dir ${objname}:"
+
+    # check if destination object already exist and create backup
+    if [ -d "${destinationObj}" ]
+    then
+        rm -Rf "${destinationObj}.save"
+        mv -f "${destinationObj}" "${destinationObj}.save"
+        echo "- existing dir saved"
+    fi
+
+    # copy directory to destination
+    cp -r "${sourceObj}" "${destinationDir}"
+    echo "- new dir copied to ${destinationDir}"
+    echo ""
+}
+
+
+# walk over all files in ./files directory
 shopt -s nullglob
 for filename in ${sourcefiles};
 do
-    copyFile ${filename} ${target}
+    # copy files
+    if [ -f "${filename}" ]
+    then
+        copyFile "${filename}" "${target}"
+    fi
+
+    # copy directories
+    if [ -d "${filename}" ]
+    then
+        copyDir "${filename}" "${target}"
+    fi
 done
 shopt -u nullglob
